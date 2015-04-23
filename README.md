a4cee1388676bdf6e30de7866a76d1aa3d29aedf
Kazemi, Darius <dkazemi@akamai.com>
2015-04-23 11:29:12 -0400

    first

---
+++ README.md
@@ -0,0 +1,10 @@
+# zealous-complete (Zealous Autocomplete)
+
+fun.
+
+## External licenses
+_This uses Ryan Schuft's [inflection.js](https://code.google.com/p/inflection-js/) which gracefully pluralizes nouns (among other things). inflection.js is covered by the license and copyright at the top of its file._
+
+## License
+Copyright (c) 2015 Kazemi, Darius  
+Licensed under the MIT license.
---
+++ js/app.js
@@ -0,0 +1,81 @@
+/* global sharing */
+Array.prototype.pick = function() {
+  return this[Math.floor(Math.random()*this.length)];
+};
+
+var states = [
+  'Alabama', 'Alaska', 'Arizona', 'Arkansas', 'California',
+  'Colorado', 'Connecticut', 'Delaware', 'Florida', 'Georgia', 
+  'Hawaii', 'Idaho', 'Illinois', 'Indiana', 'Iowa', 
+  'Kansas', 'Kentucky', 'Louisiana', 'Maine', 'Maryland', 
+  'Massachusetts', 'Michigan', 'Minnesota', 'Mississippi', 
+  'Missouri', 'Montana', 'Nebraska', 'Nevada', 'New Hampshire',
+  'New Jersey', 'New Mexico', 'New York', 'North Carolina', 
+  'North Dakota', 'Ohio', 'Oklahoma', 'Oregon', 'Pennsylvania', 
+  'Rhode Island', 'South Carolina', 'South Dakota', 'Tennessee', 
+  'Texas', 'Utah', 'Vermont', 'Virginia', 'Washington', 
+  'West Virginia', 'Wisconsin', 'Wyoming'
+];
+
+var letters='bcdfghjklmnpqrstvwxyz'.split('');
+var vowels='aeiou'.split('');
+function pair(word) {
+  word = word.toLowerCase();
+  var len = word.length - 1;
+  var endsVowel = false;
+  // if ends in space or blank, pick anything
+  if (word[len] === ' ' || word === '') {
+    return (Math.random() < 0.5) ? letters.pick()+vowels.pick() : vowels.pick()+letters.pick();
+  }
+  if (
+    word[len] === 'a' ||
+    word[len] === 'e' ||
+    word[len] === 'i' ||
+    word[len] === 'o' ||
+    word[len] === 'u'
+    ){
+    endsVowel = true;
+  }
+  return (endsVowel) ? letters.pick()+vowels.pick() : vowels.pick()+letters.pick();
+}
+
+$(function() {
+  $('.input').autocomplete({
+    appendMethod: 'replace',
+   source:[
+    function( q,add ){
+      $.ajax({
+          url: 'http://suggestqueries.google.com/complete/search',
+          jsonp: 'callback',
+          dataType: 'jsonp',
+          data: {
+            q: q+pair(q),
+            client: 'chrome'
+          },
+          success: function( response ) {
+            console.log( response ); // server response
+            response[1].length = 4;
+            add(response[1]);
+          }
+      });
+    }
+   ]
+  });
+});
+
+function generate(nounPlural, verb) {
+  nounPlural = nounPlural || nouns.pick().pluralize();
+  verb = verb || verbs.pick();
+  var generatedText = '<em>' + verb + '</em> ALL the <em>' + nounPlural + '</em>';
+  var sharedText = verb + ' ALL the ' + nounPlural;
+  $('#content').html(generatedText);
+  var shareUrl = window.location.href.split('?')[0]+'?word='+sharing.encodeStr(verb)+'$'+sharing.encodeStr(nounPlural);
+  $('#share').attr('href', shareUrl);
+  $('.twitter-share-button').remove();
+  $('#twitterShare').html('<a href="https://twitter.com/share" class="twitter-share-button" data-url="' + shareUrl + '" data-text="' + sharedText + '" data-lang="en">Tweet</a>');
+  if (twttr.widgets) {
+    twttr.widgets.load();
+  }
+}
+
+$('#generate').click(function() { generate(); });
